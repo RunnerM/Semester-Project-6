64b7a298261e4bfb68682d27cd6941ec8f30a759
Marton Pentek <pentek.marci@gmail.com>
2022-05-10 11:28:07 +0200

    adding connetcion strings

--- EFCore/Utils/Context.cs
+++ EFCore/Utils/Context.cs
@@ -5,13 +5,16 @@ namespace EFCore.Utils;
 
 public class Context : DbContext
 {
+    // sqladmin password:
+    // 7DaM5vEa!1q550H8#pFtH
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         #if MIGRATE_STAGING
-        optionsBuilder.UseSqlServer(@"Server=localhost;Database=Staging;User Id=sa;Password=123456;");
+        optionsBuilder.UseSqlServer(@"Server=tcp:sep6-sql-server.database.windows.net,1433;Initial Catalog=movie-db;Persist Security Info=False;User ID=sqladmin;Password=7DaM5vEa!1q550H8#pFtH;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
         #elif MIGRATE_PROD
-        optionsBuilder.UseSqlServer(@"Server=localhost;Database=Prod;User Id=sa;Password=123456;");
+        optionsBuilder.UseSqlServer(@"Server=tcp:sep6-sql-server.database.windows.net,1433;Initial Catalog=movie-db_prod;Persist Security Info=False;User ID=sqladmin;Password=7DaM5vEa!1q550H8#pFtH;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
         #endif
+        
     }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
