6a9a4fc2b949728739c4b5e07bb7227a6606277a
Marton Pentek <pentek.marci@gmail.com>
2022-05-10 19:42:34 +0200

    clean up template stuff plus some design changes

--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -1,6 +1,5 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
-using WebApp.Data;
 using MudBlazor.Services;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,8 +7,6 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>();
-
 builder.Services.AddMudServices();
 
 var app = builder.Build();
