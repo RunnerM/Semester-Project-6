068b84c3364d1d76db118b41c558fc31e2e27110
Marton Pentek <pentek.marci@gmail.com>
2022-05-12 18:59:44 +0200

    lets make heroku great again

--- EFCore/Program.cs
+++ EFCore/Program.cs
@@ -1,8 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-using Data.Domain;
-using EFCore.Utils;
-
-var ctx = new Context();
-var k = ctx.Set<Movie>().Single(x => x.Id == new Guid());
 Console.WriteLine("Hello, World!");
