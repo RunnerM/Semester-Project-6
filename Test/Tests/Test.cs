40bdfc5320687ca5b1efc4b5a160fa038911b426
Marton Pentek <pentek.marci@gmail.com>
2022-05-16 22:52:57 +0200

    adding auto includes and testing it

--- Test/Tests/Test.cs
+++ Test/Tests/Test.cs
@@ -1,4 +1,8 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
+using Data.Domain;
 using EFCore.Config;
 using EFCore.Utils;
 using Xunit;
@@ -14,5 +18,34 @@ public class Test
         Assert.True(Config.UnitTests);
         await using var context = new Context();
         Assert.Equal("Microsoft.EntityFrameworkCore.InMemory",context.Database.ProviderName);
+    }
+
+    [Fact]
+    public async Task TestAutoIncludes()
+    {
+        await using var context = new Context();
+        context.Add(new User()
+        {
+            Id = Guid.NewGuid(),
+            Email = "test@test.com",
+            GoogleExternalId = "id",
+            Name = "Marton",
+            TopLists = new List<UserToplists>()
+            {
+                new UserToplists()
+                {
+                    Movie = new Movie()
+                    {
+                        Id = Guid.NewGuid(),
+                        Title = "title",
+                        TMDBExternalId = "id",
+                        Year = 2000
+                    }
+                }
+            }
+        });
+        await context.SaveChangesAsync();
+        var user = context.Set<User>().First();
+        Assert.NotNull(user.TopLists);
     }
 }
